4a1fd115c0b4e596d5ecc5af5496e41b33653d3c
duriyeatay <schon_19@hotmail.com>
2025-11-25 20:33:17 +0300

    donguler-for-loop
    donguler-for-loop

---
+++ donguler-for-loop/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace donguler_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ekrandan girilen sayıya kadar olan tek sayıları ekrana yazdır.
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayac = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                    Console.WriteLine(i);
+            }
+
+            // 1 ile 1000 arasındaki tek ve çift sayıların kendi içlerinde toplamlarını ekrana yazdır.
+            int tekToplam = 0;
+            int ciftToplam = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                    tekToplam += i;
+                else
+                    ciftToplam += i;
+            }
+
+            Console.WriteLine("Tek Toplam = " + tekToplam);
+            Console.WriteLine("Çift Toplam = " + ciftToplam);
+
+            // break, continue
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    break;   // Döngüyü tamamen durdurur
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("-----");
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    continue;   // Sadece bu turu atlar, döngü devam eder
+                Console.WriteLine(i);
+}
+
+        }
+    }
+}
