fbc30b654ec5f2c5362d76639102f0c24f00d3d0
duriyeatay <schon_19@hotmail.com>
2025-11-25 19:51:43 +0300

    if-else-if
    if-else-if

---
+++ if-else-if/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Günaydın!");
+            else if (time >= 11 && time < 18)
+                Console.WriteLine("İyi Günler!");
+            else
+                Console.WriteLine("İyi Geceler!");
+
+            // Ternary (?:) ile yazım
+            string sonuc = time >= 6 && time < 11 ? "Günaydın!" :
+                           time >= 11 && time < 18 ? "İyi Günler!" :
+                           "İyi Geceler!";
+
+            Console.WriteLine(sonuc);
+        }
+    }
+}
