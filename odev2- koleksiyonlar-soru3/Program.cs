c313250bfb47eed134de7a1498ec85b09cb295d2
duriyeatay <schon_19@hotmail.com>
2025-11-26 23:08:38 +0300

    odev2-koleksiyonlar-soru3
    odev2-koleksiyonlar-soru3

---
+++ odev2- koleksiyonlar-soru3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        string sesliler = "aeıioöuüAEIİOÖUÜ";
+        ArrayList bulunanHarfler = new ArrayList();
+
+        Console.Write("Bir cümle giriniz: ");
+        string cumle = Console.ReadLine();
+
+        foreach (char ch in cumle)
+        {
+            if (sesliler.Contains(ch))
+                bulunanHarfler.Add(ch);
+        }
+
+        bulunanHarfler.Sort();
+
+        Console.WriteLine("\nCümledeki sesli harfler (sıralanmış): ");
+        foreach (var harf in bulunanHarfler)
+            Console.Write(harf + " ");
+
+        Console.WriteLine($"\n\nToplam Sesli Harf Sayısı: {bulunanHarfler.Count}");
+    }
+}
+
